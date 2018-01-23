6d57da79ecb99a95b1af4a5b37002a7193344817
Roland Harangozo <roleee3@gmail.com>
2018-01-22 21:07:54 -0600

    .

--- ann/ann.py
+++ ann/ann.py
@@ -6,8 +6,11 @@ class ANN:
     w = []
     a = []
     z = []
+    training = []
+    delta = []
+    derivates = []
 
-    def __init__(self, layers):
+    def __init__(self, layers, trainingSet):
         self.layers = layers
 
         for i in range(0, len(layers)-1):
@@ -16,6 +19,10 @@ class ANN:
 
         self.a = [None] * len(layers)
         self.z = [None] * len(layers)
+        self.derivates = [None] * len(layers)
+        self.delta = [None] * (len(layers) + 1)
+
+        self.training = trainingSet
 
     def setInput(self, x):
         self.a[0] = x
@@ -39,8 +46,76 @@ class ANN:
             self.computeA(i+1)
         return self.a[len(self.layers)-1]
 
+    def sigmoidDet(self, x):
+        return math.exp(-x) / (1 + math.exp(-x)) ** 2
+
+    def computeDelta(self, index, y):
+        if(index == len(self.layers) - 1):
+            matrix = - (y - self.a[len(self.layers)-1])
+            print('matrix:')
+            print(matrix)
+            for r in range(0,len(matrix.A)):
+                for c in range(0, len(matrix.A[0])):
+                    matrix.A[r][c] = matrix.A[r][c] * self.sigmoidDet(self.z[index].A[r][c])
+            print('post matrix:')
+            print(matrix)
+
+            self.delta[index] = matrix
+            return self.delta[index]
+        else:
+            matrix = self.delta[index+1] * self.w[index].T
+            for r in range(0,len(matrix.A)):
+                for c in range(0, len(matrix.A[0])):
+                    matrix.A[r][c] = matrix.A[r][c] * self.sigmoidDet(self.z[index].A[r][c])
+            self.delta[index] = matrix
+            return self.delta[index]
+
+    def computeDeltas(self, y):
+        for i in range(1, len(self.layers)):
+            self.computeDelta(len(self.layers)-i, y)
+
+    def computeDerivates(self):
+        for i in range(0, len(self.layers)-1):
+            print('aT:')
+            print(self.a[i].T)
+            print('delta')
+            print(self.delta[i+1])
+            self.derivates[i] = self.a[i].T * self.delta[i+1]
+            print('deriv')
+            print(self.derivates[i])
+
+    def learn(self, scalar):
+        for i in range(0, len(self.layers)-1):
+            print('deriv')
+            print(self.derivates[i])
+            print('pre')
+            print(self.w[i])
+            self.w[i] = self.w[i] + scalar * self.derivates[i]
+            print('post')
+            print(self.w[i])
 
 
-ann = ANN([2,3,1])
-ann.setInput(np.matrix([0.5,0.5]))
+# ann = ANN([2,3,2],[])
+# ann.setInput(np.matrix([[0.5,0.5],[0.4,0.5]]))
+# print(ann.compute())
+# print(ann.cost([[0.5,0.5],[0.5,0.5]]))
+# print('--')
+# print(ann.computeDelta(2, [[0.5,0.5],[0.5,0.5]]))
+# print(ann.computeDelta(1, [[0.5,0.5],[0.5,0.5]]))
+
+ann = ANN([1,3,1],[])
+ann.setInput(np.matrix([[0.1],[0.8]]))
 print(ann.compute())
+# print('cost:')
+# print(ann.cost([[0.5],[0.5]]))
+# print('deltas')
+# ann.computeDeltas([[0.8],[0.2]])
+# ann.computeDerivates()
+
+for i in range(0,100):
+    ann.computeDeltas([[0.8],[0.2]])
+    ann.computeDerivates()
+    ann.learn(2)
+print(ann.compute())
+
+# ann.costFunction([[0.5],[0.5]])
