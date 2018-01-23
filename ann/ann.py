b31b8f1306ac79ed52d296925fb4b80524fc75cf
Roland Harangozo <roleee3@gmail.com>
2018-01-22 22:32:13 -0600

    Finetune

--- ann/ann.py
+++ ann/ann.py
@@ -6,23 +6,21 @@ class ANN:
     w = []
     a = []
     z = []
-    training = []
     delta = []
     derivates = []
 
-    def __init__(self, layers, trainingSet):
+    def __init__(self, layers):
         self.layers = layers
 
         for i in range(0, len(layers)-1):
             matrix = np.matrix(np.random.random((layers[i], layers[i+1])))
+            # matrix = np.matrix(np.full((layers[i], layers[i+1]),0.5))
             self.w.append(matrix)
 
         self.a = [None] * len(layers)
         self.z = [None] * len(layers)
         self.derivates = [None] * len(layers)
         self.delta = [None] * (len(layers) + 1)
-
-        self.training = trainingSet
 
     def setInput(self, x):
         self.a[0] = x
@@ -52,14 +50,9 @@ class ANN:
     def computeDelta(self, index, y):
         if(index == len(self.layers) - 1):
             matrix = - (y - self.a[len(self.layers)-1])
-            print('matrix:')
-            print(matrix)
             for r in range(0,len(matrix.A)):
                 for c in range(0, len(matrix.A[0])):
                     matrix.A[r][c] = matrix.A[r][c] * self.sigmoidDet(self.z[index].A[r][c])
-            print('post matrix:')
-            print(matrix)
-
             self.delta[index] = matrix
             return self.delta[index]
         else:
@@ -76,46 +69,20 @@ class ANN:
 
     def computeDerivates(self):
         for i in range(0, len(self.layers)-1):
-            print('aT:')
-            print(self.a[i].T)
-            print('delta')
-            print(self.delta[i+1])
             self.derivates[i] = self.a[i].T * self.delta[i+1]
-            print('deriv')
-            print(self.derivates[i])
 
     def learn(self, scalar):
         for i in range(0, len(self.layers)-1):
-            print('deriv')
-            print(self.derivates[i])
-            print('pre')
-            print(self.w[i])
             self.w[i] = self.w[i] + scalar * self.derivates[i]
-            print('post')
-            print(self.w[i])
 
-
-# ann = ANN([2,3,2],[])
-# ann.setInput(np.matrix([[0.5,0.5],[0.4,0.5]]))
-# print(ann.compute())
-# print(ann.cost([[0.5,0.5],[0.5,0.5]]))
-# print('--')
-# print(ann.computeDelta(2, [[0.5,0.5],[0.5,0.5]]))
-# print(ann.computeDelta(1, [[0.5,0.5],[0.5,0.5]]))
-
-ann = ANN([1,3,1],[])
-ann.setInput(np.matrix([[0.1],[0.8]]))
+ann = ANN([1,3,1])
+ann.setInput(np.matrix([[0.3],[0.8]]))
+print('Output without learning:')
 print(ann.compute())
-# print('cost:')
-# print(ann.cost([[0.5],[0.5]]))
-# print('deltas')
-# ann.computeDeltas([[0.8],[0.2]])
-# ann.computeDerivates()
-
-for i in range(0,100):
+print('-=learning=-')
+for i in range(0,15):
     ann.computeDeltas([[0.8],[0.2]])
     ann.computeDerivates()
-    ann.learn(2)
+    ann.learn(3)
+print('Output with learning: (expected would be ~0.8 / ~0.2)')
 print(ann.compute())
-
-# ann.costFunction([[0.5],[0.5]])
